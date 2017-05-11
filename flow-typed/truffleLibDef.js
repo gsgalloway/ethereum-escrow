688a56b5f01fd011e04aa5c1127578c52d6f4b87
Gavi Galloway <gavilan@qadium.com>
2017-05-11 15:11:46 -0700

    Adding flow-types for Truffle Contract and ContractInstance

--- flow-typed/truffleLibDef.js
+++ flow-typed/truffleLibDef.js
@@ -10,3 +10,33 @@ declare function contract(
     description: string,
     testBody: (accounts: string[]) => void
 ): void;
+
+
+declare type TxParams = {};
+
+
+// Documentation at https://github.com/trufflesuite/truffle-contract-schema#schema
+declare class ContractInstance {
+    contract_name?: string;
+    abi: {}[];
+    unlinked_binary: string;
+    network_id?: string | number;
+    address?: string;
+    links?: {};
+    events?: {};
+    default_network: string | number;
+    networks?: {[string]: {}};
+    address: string;
+    transactionHash: string | null;
+    contract: Contract;
+};
+
+
+// Documentation at https://github.com/trufflesuite/truffle-contract#contract-abstraction-api
+declare class Contract<InstanceT: ContractInstance> {
+    new(instanceArgs?: any[], txParams?: TxParams): Promise<InstanceT>;
+    at(address: string): Promise<InstanceT>;
+    deployed(): Promise<InstanceT>;
+    link(instance: ContractInstance): void;
+    networks(): string[];
+};
