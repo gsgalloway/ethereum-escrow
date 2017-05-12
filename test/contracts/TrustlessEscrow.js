2a38b6efabc878ad7a4fd7cb0485b364d410ddd7
Gavi Galloway <gavilan@qadium.com>
2017-05-11 20:13:11 -0700

    Working test for TrustlessEscrow#createAgreement

--- test/contracts/TrustlessEscrow.js
+++ test/contracts/TrustlessEscrow.js
@@ -1,10 +1,58 @@
 // @flow
+
+// Injected by Truffle framework
 declare var artifacts: Artifacts;
 
-let TrustlessEscrow = artifacts.require("TrustlessEscrow");
+let trustlessEscrowContract: Contract<TrustlessEscrowInstance> = artifacts.require("TrustlessEscrow");
 
 contract('TrustlessEscrow', function(accounts: string[]): void {
-    it("should do something", function() {
-        let myvar: string = "foo";
+    let buyer = accounts[0];
+    let seller = accounts[1];
+    describe('#createAgreement', function() {
+        it('should work with valid parameters', function() {
+            let instance: TrustlessEscrowInstance;
+            let value = 100;
+            return trustlessEscrowContract.deployed()
+                .then((_instance: TrustlessEscrowInstance) => {
+                    instance = _instance;
+                    return _instance.createAgreement(buyer, seller, value)
+                })
+                .then((result: TransactionResult) => {
+                    assert.isNotNull(result.tx);
+                    return instance.numAgreements.call()
+                })
+                .then((numAgreements: BigNumber) => {
+                    assert.equal(numAgreements.toNumber(), 1);
+                    return instance.agreements.call(0);
+                })
+                .then((agreementStruct: any[]) => {
+                    let observedBuyer: string = agreementStruct[0];
+                    let observedSeller: string = agreementStruct[1];
+                    let observedValue: BigNumber = agreementStruct[2];
+                    let buyerConfirmedAgreement: bool = agreementStruct[3];
+                    let sellerConfirmedAgreement: bool = agreementStruct[4];
+                    assert.equal(observedBuyer, buyer);
+                    assert.equal(observedSeller, seller);
+                    assert.equal(observedValue.toNumber(), value);
+                    assert.isFalse(buyerConfirmedAgreement);
+                    assert.isFalse(sellerConfirmedAgreement);
+                    return instance.numAgreementsPerParticipant.call(buyer);
+                })
+                .then((numAgreementsForBuyer: BigNumber) => {
+                    assert.equal(numAgreementsForBuyer.toNumber(), 1);
+                    return instance.numAgreementsPerParticipant.call(seller);
+                })
+                .then((numAgreementsForSeller: BigNumber) => {
+                    assert.equal(numAgreementsForSeller.toNumber(), 1);
+                    return instance.agreementIdsByParticipant.call(buyer, 0);
+                })
+                .then((buyersFirstAgreementId: BigNumber) => {
+                    assert.equal(buyersFirstAgreementId.toNumber(), 0);
+                    return instance.agreementIdsByParticipant.call(seller, 0);
+                })
+                .then((sellersFirstAgreementId: BigNumber) => {
+                    assert.equal(sellersFirstAgreementId.toNumber(), 0);
+                });
+        });
     });
 });
